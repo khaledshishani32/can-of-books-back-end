a11680045e9ca58160f23c3a6efba250ce898add
khaled al shishani <khaled.shishani32@gmail.com>
2021-06-22 17:29:54 +0300

    finish lab 13

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const app = express() // initialize your express app instance
 const axios = require('axios');
 const mongoose = require('mongoose');
 require('dotenv').config();
+app.use(express.json());
+const {
+    getbooks,
+    createbooks,
+    updatebooks,
+    deletebooks
+} = require('./controllers/User.controller');
 const {seedUserData} = require('./model/User.model')
 const userController= require('./controllers/User.controller')
-const {userModel}= require('./model/User.model')
+//const {userModel}= require('./model/User.model')
 
  
 const PORT = process.env.PORT
@@ -19,15 +26,20 @@ mongoose.connect('mongodb://localhost:27017/FavoriteBooks',
     { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
-seedUserData();
+//seedUserData();
 
 
-app.get('/books', userController)
+app.get('/books', getbooks)
 
 app.get('/', // our endpoint name
  function (req, res) { // callback function of what we should do with our request
   res.send('Hello World') // our endpoint function response
 })
+app.post('/book', createbooks);
+// Update route, will will receive the cat id that we want to update, and its info in the body payload
+app.put('/book/:book_idx', updatebooks);
+// Delete route, which will delete the cat by its index
+app.delete('/book/:book_idx', deletebooks)
  
  app.listen(PORT ,()=>{
      console.log(`running on port ${PORT}`)
