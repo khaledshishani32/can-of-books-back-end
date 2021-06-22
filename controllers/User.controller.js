d815e24f04b42d322aa9c1651eb02d22f552476b
khaled al shishani <khaled.shishani32@gmail.com>
2021-06-22 11:34:29 +0300

    new

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -2,13 +2,13 @@
 'use strict'
 
 
-const userModel = require('../model/User.model');
+const {userModel} = require('../model/User.model');
 
 const getBooks = (req , res )=>{
 
     const { email } = req.query;
 
-    userModel.find({ email: email }, (error, user) => {
+    userModel.findOne({ email: email }, (error, user) => {
         if (error) {
             res.send(error)
         } else {
